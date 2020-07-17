bc47f147fb432538fa24e92eb2008990ba1d82f1
kaan.sonmezoz <kaan.sonmezoz@trendyol.com>
2020-07-17 23:21:08 +0300

    Implemented 2nd test case.    - Given "1"  When add() is called Then it should return 1    - Added if statement to check whether number is empty.

--- src/main/java/com/sonmezoz/kaan/kata/tdd/case1/StringCalculator.java
+++ src/main/java/com/sonmezoz/kaan/kata/tdd/case1/StringCalculator.java
@@ -1,7 +1,13 @@
 package com.sonmezoz.kaan.kata.tdd.case1;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class StringCalculator {
     public int add(String numbers) {
-        return 0;
+        if (StringUtils.isEmpty(numbers)){
+            return 0;
+        }
+
+        return Integer.parseInt(numbers);
     }
 }
