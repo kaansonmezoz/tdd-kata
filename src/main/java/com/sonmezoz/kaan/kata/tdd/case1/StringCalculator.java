3de0ee13e25ff51f04806ede1c0a45aaa0f69fb1
kaansonmezoz <sonmezozkaan@gmail.com>
2020-07-18 23:01:32 +0300

    Consolidated conditional expressions and renamed method: add => sum

--- src/main/java/com/sonmezoz/kaan/kata/tdd/case1/StringCalculator.java
+++ src/main/java/com/sonmezoz/kaan/kata/tdd/case1/StringCalculator.java
@@ -4,15 +4,11 @@ import org.apache.commons.lang3.StringUtils;
 
 public class StringCalculator {
 
-    public int add(String numbers) {
-        if (StringUtils.isEmpty(numbers)) {
-            return 0;
-        }
-
-        return add(new Numbers(numbers));
+    public int sum(String numbers) {
+        return StringUtils.isEmpty(numbers) ? 0 : sum(new Numbers(numbers));
     }
 
-    private int add(Numbers numbers) {
+    private int sum(Numbers numbers) {
         numbers.getNegativeNumber().ifPresent((i) -> {  throw new NegativeNumberException(i); });
         return numbers.getNumbersStream().sum();
     }
